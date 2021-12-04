bd68f9aa145611df892f84933ac8520f43bf8750
calixto cotta <ccottav14@gmail.com>
2021-12-04 16:39:40 -0500

    Inicio de sesion

---
+++ com/src/main/resources/static/assets/js/iniciosesion.js
@@ -0,0 +1,69 @@
+/**
+ * 
+ * login
+ */
+ $("#formulario").validate({
+    rules: {
+        email: {
+            required: true,
+            minlength: 3,
+            maxlength: 50,
+            email: true,
+        },
+        password: {
+            required: true,
+            minlength: 6,
+            maxlength: 16,
+        }
+    }
+})
+/**
+ * 
+ */
+
+$("#login").click(function() {
+    if ($("#formulario").valid() == false) {
+        return;
+    }else{
+        //console.log("Entro aqui");
+        traerUsuarios();
+    }
+})
+
+/**
+ * 
+ */
+function traerUsuarios() {
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+
+    $.ajax({
+        url: "http://144.22.228.79:80/api/user/" + email+"/" + password,
+        type: "GET",
+        datatype: "JSON",
+        success: function(response) {
+            console.log(response)
+                swal("Registro exitoso", "En unos segundos sera redireccionado!", "success");
+
+            //Guardar datos de usuario
+            let data = {
+                'id': response.id,
+                'name' : response.name
+            };
+            //Guardo los datos en un almacenamiento loca con el nombre de object_name y le envio los datos como js
+            localStorage.setItem("object_name", JSON.stringify(data));
+            setTimeout(
+                function(){ 
+                    $(document).ready(function(){
+                        $(location).attr('href',"menuUser.html");
+                    });
+                }, 10000
+            );
+            
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            swal("Validación", "Error en la aplicacion, comuniquese conel administrador del sistema", "error");
+        }
+    });
+}
